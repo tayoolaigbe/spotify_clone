3b20d9450996fbf1d5530c4a8accc255b5ce168c
tayoolaigbe <tayoolaigbe@outlook.com>
2023-06-15 00:11:22 +0100

    COmpleted the MediaItem component

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -2,6 +2,7 @@
 
 import useLoadImage from '@/hooks/useLoadImage';
 import { Song } from '@/types';
+import Image from 'next/image';
 
 interface MediaItemProps {
 	data: Song;
@@ -22,7 +23,18 @@ const MediaItem: React.FC<MediaItemProps> = ({ data, onClick }) => {
 			onClick={handleClick}
 			className="flex items-center gap-x-3 cursor-pointer hover:bg-neutral-800/50 w-full p-2 rounded-md"
 		>
-			MediaItem
+			<div className="relative rounded-md min-h-[48px] min-w-[48px] overflow-hidden">
+				<Image
+					fill
+					src={imageUrl || '/images/liked.png'}
+					alt="Media Item"
+					className="object-cover"
+				/>
+			</div>
+			<div className="flex flex-col gap-y-1 overflow-hidden">
+				<p className="text-white truncate">{data.title}</p>
+				<p className="text-neutral-400 text-sm truncate">By {data.author}</p>
+			</div>
 		</div>
 	);
 };
